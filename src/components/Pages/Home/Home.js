89d3909b42f7adc0c36abcab0451881933b12c19
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-11 00:43:21 +0600

    makeAppintment section added

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Banner from "./Banner";
+import DentalCare from "./DentalCare";
 import Info from "./Info";
+import MakeAppointment from "./MakeAppointment";
 import Services from "./Services";
 
 const Home = () => {
   return (
     <div>
       <Banner />
-      <Info/>
-      <Services/>
+      <Info />
+      <Services />
+      <DentalCare />
+      <MakeAppointment/>
     </div>
   );
 };
---
+++ src/components/Pages/Home/MakeAppointment.js
@@ -0,0 +1,35 @@
+import React from "react";
+import doctor from "../../../assets/images/doctor-small.png";
+import appointmentImg from "../../../assets/images/appointment.png";
+
+const MakeAppointment = () => {
+  return (
+    <section
+      style={{ background: `url(${appointmentImg})` }}
+      className="flex justify-center items-center mt-52"
+    >
+      <div className="flex-1 mt-[-100px]">
+        <img src={doctor} alt="doctor" />
+      </div>
+      <div className="flex-1">
+        <p className="text-primary font-bold">Appointment</p>
+        <h2 className="text-4xl font-semibold text-white my-4">
+          Make an appointment Today
+        </h2>
+        <p className="text-white my-7">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsumis that it has a more-or-less normal distribution of
+          letters,as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page
+        </p>
+        <button className="uppercase btn btn-primary bg-gradient-to-r from-secondary to-primary text-white">
+          Get Started
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default MakeAppointment;
