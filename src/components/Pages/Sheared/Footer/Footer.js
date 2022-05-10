7cd19ed94c52b8b6648b57e5b570a0a6ef7b2186
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-10 18:32:05 +0600

    first commit

---
+++ src/components/Pages/Sheared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div>
+      <h2>footer</h2>
+    </div>
+  );
+};
+
+export default Footer;
