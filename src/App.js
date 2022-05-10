5e41e75b746f7a2d09ae9b0b06427d8aa15331d5
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-10 19:11:12 +0600

    navbar added route setup done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./components/Pages/About/About";
+import Appointment from "./components/Pages/Appointment/Appointment";
+import Contact from "./components/Pages/Contact/Contact";
 import Home from "./components/Pages/Home/Home";
+import LogIn from "./components/Pages/Login/LogIn/LogIn";
+import SignUp from "./components/Pages/Login/Signup/SignUp";
+import Reviews from "./components/Pages/Reviews/Reviews";
 import Footer from "./components/Pages/Sheared/Footer/Footer";
 import Header from "./components/Pages/Sheared/Header/Header";
 
@@ -12,6 +17,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/appointment" element={<Appointment />}></Route>
+        <Route path="/reviews" element={<Reviews />}></Route>
+        <Route path="/login" element={<LogIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
       </Routes>
       <Footer />
     </div>
